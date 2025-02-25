db9073996f092f821333333733dda997b0de8d52
DavidWells <hello@davidwells.io>
2025-02-25 14:44:43 -0800

    deploy

--- src/utils/generate-site.js
+++ src/utils/generate-site.js
@@ -1,40 +1,25 @@
 import fs from 'fs-extra'
 import path from 'path'
-import { getSavedJSONFileData } from './fs.js'
+import { getSavedJSONFileData, getSavedMdFileData } from './fs.js'
 import { createStarTable } from './generate-readme.js'
 import { SITE_DIRECTORY } from '../_constants.js'
 
 async function generateStaticSite() {
   try {
     // Get the stars data
-    const allStars = await getSavedJSONFileData()
+    const allStars = (await getSavedMdFileData()).map(({ frontmatter }) => {
+      return frontmatter
+    })
+    console.log('getAllStars', allStars.length)
 
-    // Sort by starred date
-    const sortedByStarredDate = allStars
-      .sort((a, b) => new Date(b.starred_at) - new Date(a.starred_at))
-      .map(({ repo, starred_at }) => {
-        const licenseObj = repo.license || {}
+    let sortedByStarredDate = allStars
+      .sort((a, b) => new Date(b.starredAt) - new Date(a.starredAt))
+      .map((repo) => {
         return {
-          repo: repo.full_name,
-          private: repo.private,
-          description: repo.description,
-          starredAt: starred_at,
-          createdAt: repo.created_at,
-          url: repo.html_url,
-          language: repo.language,
-          stars: repo.stargazers_count,
-          branch: repo.default_branch,
-          license: licenseObj.spdx_id,
-          visibility: repo.visibility,
-          isFork: repo.fork,
-          isPrivate: repo.private,
-          isArchived: repo.archived,
-          isTemplate: repo.is_template,
-          isDisabled: repo.disabled,
-          topics: repo.topics,
+          ...repo,
+          isPrivate: repo.hasOwnProperty('isPublic') ? !repo.isPublic : false,
         }
       })
-      .filter((repo) => !repo.private) // Exclude private repos
 
     // Generate the table HTML
     const tableHtml = createStarTable(sortedByStarredDate, false, 130)
